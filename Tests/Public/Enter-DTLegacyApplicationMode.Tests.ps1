2ee14a0a14d4c347d8facb65a6d54e33735846cb
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2023-03-19 22:54:33 +0900

    Add tests

---
+++ Tests/Public/Enter-DTLegacyApplicationMode.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Describe 'Enter-DTLegacyApplicationMode' {
+    BeforeEach {
+        Import-Module $PSScriptRoot\..\..\DynamicTitle -Force
+    }
+
+    It 'should not throw an error' {
+        Enter-DTLegacyApplicationMode
+    }
+
+    AfterEach {
+        Remove-Module DynamicTitle -Force
+    }
+}
---
+++ Tests/Public/Exit-DTLegacyApplicationMode.Tests.ps1
@@ -0,0 +1,14 @@
+﻿Describe 'Exit-DTLegacyApplicationMode' {
+    BeforeEach {
+        Import-Module $PSScriptRoot\..\..\DynamicTitle -Force
+    }
+
+    It 'should not throw an error' {
+        Enter-DTLegacyApplicationMode
+        Exit-DTLegacyApplicationMode
+    }
+
+    AfterEach {
+        Remove-Module DynamicTitle -Force
+    }
+}
