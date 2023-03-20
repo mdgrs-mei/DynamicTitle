778f6eced66ecc00bfff405dc84b004cf140801f
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2023-03-20 22:37:56 +0900

    Update help

--- DynamicTitle/Public/Enter-DTLegacyApplicationMode.ps1
+++ DynamicTitle/Public/Enter-DTLegacyApplicationMode.ps1
@@ -5,9 +5,8 @@ Calls $host.NotifyBeginApplication() so that the host doesn't reset the title wh
 .DESCRIPTION
 Calls $host.NotifyBeginApplication() so that the host doesn't reset the title when executing legacy command line applications.
 
-When command line application are executed on the main thread, the host saves the title string before calling the application and resets it when the application returns.
-This behavior sometimes causes a blink on the title as you are changing the title on a background thread.
-Commands between Enter-DTLegacyApplicationMode and Exit-DTLegacyApplicationMode are all treated as legacy command line applications so the title reset issue does not happen.
+When command line applications are executed on the main thread, the host saves the title string before calling the application and resets it when the application returns. This behavior sometimes causes a blink on the title as you are changing the title on a background thread.
+Enter-DTLegacyApplicationMode calls $host.NotifyBeginApplication() to notify the host that the subsequent commands are all legacy command line applications, and therefore the title reset behavior on every command call is suppressed.
 Note that this workaround stops all the state restore from the command line applications which may cause some other issues.
 
 .INPUTS
