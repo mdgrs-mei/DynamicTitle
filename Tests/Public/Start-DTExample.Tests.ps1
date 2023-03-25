8344d7564fcbefa6db1176365d11bc67a7b029e6
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2023-03-25 23:33:28 +0900

    Add test for Start-DTExample

---
+++ Tests/Public/Start-DTExample.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Describe 'Start-DTExample' {
+    BeforeEach {
+        Import-Module $PSScriptRoot\..\..\DynamicTitle -Force
+    }
+
+    It 'should not throw an error' {
+        Start-DTExample -Name CommandExecutionTime
+    }
+
+    AfterEach {
+        Remove-Module DynamicTitle -Force
+    }
+}
