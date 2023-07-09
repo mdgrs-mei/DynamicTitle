96ff11284943a9f9cdc999b80c247fd5dea4b19b
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2023-07-09 22:23:09 +0900

    Add Spinner example

---
+++ DynamicTitle/Examples/Spinner.ps1
@@ -0,0 +1,60 @@
+﻿#Requires -Modules DynamicTitle
+
+# Suppress this for $initializationScript
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+
+$modulePath = Join-Path (Get-Module DynamicTitle).ModuleBase 'DynamicTitle.psd1'
+
+$commandStartJob = Start-DTJobCommandPreExecutionCallback -ScriptBlock {
+    Get-Date
+}
+
+$commandEndJob = Start-DTJobPromptCallback -ScriptBlock {
+    Get-Date
+}
+
+$initializationScript = {
+    param ($modulePath)
+    Import-Module $modulePath
+    $spinnerSymbols = @('🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘')
+    $spinnerSymbolIndex = 0
+}
+
+$scriptBlock = {
+    param($commandStartJob, $commandEndJob)
+    $commandStartDate = Get-DTJobLatestOutput $commandStartJob
+    $commandEndDate = Get-DTJobLatestOutput $commandEndJob
+    if ($null -ne $commandStartDate)
+    {
+        if (($null -eq $commandEndDate) -or ($commandEndDate -lt $commandStartDate))
+        {
+            $commandDuration = (Get-Date) - $commandStartDate
+        }
+    }
+
+    $spinner = $spinnerSymbols[0]
+    if ($commandDuration)
+    {
+        if ($commandDuration.TotalSeconds -gt 1)
+        {
+            $script:spinnerSymbolIndex = ($script:spinnerSymbolIndex + 1) % $spinnerSymbols.Count
+            $spinner = $spinnerSymbols[$script:spinnerSymbolIndex]
+        }
+    }
+    else
+    {
+        $script:spinnerSymbolIndex = 0
+    }
+
+    '{0} PowerShell' -f $spinner
+}
+
+$params = @{
+    ScriptBlock = $scriptBlock
+    ArgumentList = $commandStartJob, $commandEndJob
+    InitializationScript = $initializationScript
+    InitializationArgumentList = $modulePath
+}
+
+Start-DTTitle @params
